f4f5804f880ef2db6dbeb57c4632553c9893c315
zp4rker <me@zp4rker.com>
2019-12-09 09:27:52 +1100

    testing hopper identification

--- src/main/kotlin/com/zp4rker/bettercrophoppers/BetterCropHoppers.kt
+++ src/main/kotlin/com/zp4rker/bettercrophoppers/BetterCropHoppers.kt
@@ -1,11 +1,24 @@
 package com.zp4rker.bettercrophoppers
 
+import org.bukkit.block.Hopper
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class BetterCropHoppers : JavaPlugin() {
 
     override fun onEnable() {
         logger.info("Successfully enabled!")
+        server.pluginManager.registerEvents(InteractListener, this)
     }
 
+}
+
+object InteractListener: Listener {
+    @EventHandler
+    fun onInteract(event: PlayerInteractEvent) {
+        if (event.clickedBlock is Hopper) event.player.sendMessage("You found a hopper!")
+        else event.player.sendMessage("That's not a hopper!")
+    }
 }
