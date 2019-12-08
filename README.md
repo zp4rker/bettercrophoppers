b91d2b46ea85dcdcf628cbb70b8788312c46bbef
zp4rker <me@zp4rker.com>
2019-12-09 09:20:07 +1100

    setup project

---
+++ README.md
@@ -0,0 +1,2 @@
+# bettercrophoppers
+ A crophopper plugin for 1.8 that actually works, doesn't throw errors in console and isn't slower than a snail.
---
+++ src/main/kotlin/com/zp4rker/bettercrophoppers/BetterCropHoppers.kt
@@ -0,0 +1,11 @@
+package com.zp4rker.bettercrophoppers
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BetterCropHoppers : JavaPlugin() {
+
+    override fun onEnable() {
+        logger.info("Successfully enabled!")
+    }
+
+}
