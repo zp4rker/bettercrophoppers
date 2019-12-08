a0e5ce8c8811395b9e30d29fcb3583baa894f957
zp4rker <me@zp4rker.com>
2019-12-09 09:58:54 +1100

    testing custom hopper identification

--- src/main/kotlin/com/zp4rker/bettercrophoppers/BetterCropHoppers.kt
+++ src/main/kotlin/com/zp4rker/bettercrophoppers/BetterCropHoppers.kt
@@ -3,22 +3,35 @@ package com.zp4rker.bettercrophoppers
 import org.bukkit.block.Hopper
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.metadata.FixedMetadataValue
 import org.bukkit.plugin.java.JavaPlugin
 
 class BetterCropHoppers : JavaPlugin() {
 
     override fun onEnable() {
         logger.info("Successfully enabled!")
-        server.pluginManager.registerEvents(InteractListener, this)
+        server.pluginManager.registerEvents(TestListener(this), this)
     }
 
 }
 
-object InteractListener: Listener {
+class TestListener(private val plugin: JavaPlugin): Listener {
+    @EventHandler
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        if (event.blockPlaced.state is Hopper) {
+            event.blockPlaced.setMetadata("crophopper", FixedMetadataValue(plugin, true))
+        }
+    }
+
     @EventHandler
     fun onInteract(event: PlayerInteractEvent) {
-        if (event.clickedBlock is Hopper) event.player.sendMessage("You found a hopper!")
-        else event.player.sendMessage("That's not a hopper!")
+        if (event.clickedBlock.state !is Hopper) return
+
+        val hopper = event.clickedBlock.state as Hopper
+        if (!hopper.hasMetadata("crophopper") || !hopper.getMetadata("crophopper")[0].asBoolean()) return
+
+        event.player.sendMessage("You found a custom hopper, well done!")
     }
 }
